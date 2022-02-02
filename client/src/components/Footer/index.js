8b7197efb3505b8a56286342f23285b1c1a12fb7
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2022-02-01 20:00:18 -0600

    updated styles

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container">&copy;2020 by Lernantino</div>
+    <footer className="w-100 mt-auto bg-dark p-4">
+      <div className="container">&copy;2020 by Rob Atalla</div>
     </footer>
   );
 };
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className="bg-secondary mb-4 py-2 flex-row align-center">
+    <header className="bg-dark mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <Link
           to={`/`}
           style={{ fontWeight: 700 }}
           className="text-light"
         >
-          <h1>Deep Thoughts</h1>
+          <h1>Deep Thoughts...</h1>
         </Link>{' '}
       </div>
     </header>
